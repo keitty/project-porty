a4fbc4bfaafd69f59ee5fb4ff525a9c86f1eeb13
keitty <keitty.prevedel@gmail.com>
2026-04-11 21:10:52 -0300

    Remove h2 from Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <nav className="bg-lime-300 text-black p-4 flex justify-between items-center">
-      <h2 className="font-bold text-xl">Project Porty</h2>
+      
       <ul className="flex gap-6">
         <li><a href="#about" className="hover:text-lime-700 cursor-pointer font-medium">About</a></li>
         <li><a href="#experience" className="hover:text-lime-700 cursor-pointer font-medium">Experience</a></li>
